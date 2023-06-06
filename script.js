803f3a08b958b8cec4257212de790e797739ed05
Alexander Bobykin <birmudo@gmail.com>
2023-06-06 10:46:17 +0600

    Trying changes to make it work better on deploy host

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-const tag = document.createElement('script')
+var tag = document.createElement('script')
 
 tag.src = '//www.youtube.com/iframe_api'
-const firstScriptTag = document.getElementsByTagName('script')[0]
+var firstScriptTag = document.getElementsByTagName('script')[0]
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
 
-let player
+var player
 
 function onYouTubeIframeAPIReady() {
-  player = new YT.Player('ytplayer', {
+  player = new YT.Player('#ytplayer', {
     events: {
       onReady: onPlayerReady,
+      onStateChange: onPlayerStateChange,
     },
   })
 }
 
 function onPlayerReady() {
+  console.log('onPlayerReady triggered')
+  player.playVideo()
+  player.unMute()
+}
+
+function onPlayerStateChange() {
+  console.log('onPlayerStateChange works')
   player.unMute()
 }
